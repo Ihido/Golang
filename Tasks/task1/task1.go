bb812553259ac80d9b3e2190933b7bbc2b8109b5
Ihido <ihido@bk.ru>
2025-10-16 19:43:57 +0500

    Первые попытки написать код

--- Tasks/task1/task1.go
+++ Tasks/task1/task1.go
@@ -3,5 +3,27 @@ package main
 import "fmt" //пакет функционала для форматирования и вывода данныхх в консоль.
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println()
+}
+
+var a, b, c string
+
+func test() {
+	var hello string
+	hello = "My name"
+	fmt.Println(hello)
+}
+
+func test1() {
+	var (
+		name string = "Mitya"
+		age  int    = 23
+	)
+	fmt.Println(name)
+	fmt.Println(age)
+}
+
+func integer() {
+	var a int = 1
+	fmt.Println(a)
 }
