1aa7b560601c09229957828f9cff6dc993bfc46a
Ihido <ihido@bk.ru>
2025-10-17 21:41:04 +0500

    Решил 2ой блок задач

---
+++ Tasks/task2/task2.go
@@ -0,0 +1,32 @@
+package main
+
+//-----------------------------------------------------1
+
+/*func main() {
+	var D, W, P, S int
+	fmt.Println("Введите длину: ")
+	fmt.Scanln(&D)
+	fmt.Println("Введите ширину: ")
+	fmt.Scanln(&W)
+	P = 2 * (D + W)
+	S = D * W
+	fmt.Println("Периметр:", P, "Площадь:", S)
+}
+*/
+
+/*func main() {
+	var a, b, c, S int
+	fmt.Println("Введите три числа через пробел: ")
+	fmt.Scanln(&a, &b, &c)
+	S = (a + b + c) / 3
+	fmt.Println("Среднее арифметическое:", S)
+}*/
+
+//-----------------------------------------------------3
+
+/*func main() {
+	var a, b int
+	fmt.Println("Введите два числа через пробел: ")
+	fmt.Scanln(&a, &b)
+	fmt.Println("Вывод:", b, a)
+}*/
