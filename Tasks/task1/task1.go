0593861ac2223ec59188290c7963424a5591b982
Ihido <Ihido@bk.ru>
2025-10-17 09:54:40 +0500

    Решил базовые задачи

--- Tasks/task1/task1.go
+++ Tasks/task1/task1.go
@@ -1,29 +1,42 @@
 package main
 
-import "fmt" //пакет функционала для форматирования и вывода данныхх в консоль.
+//-----------------------------------------------------1
+/*func main() {
+    var name string
 
+    fmt.Print("Пожалуйста, введите ваше имя: ")
+    fmt.Scanln(&name)
+
+    fmt.Printf("Привет, %s! Добро пожаловать!\n", name)
+} */
+
+//-----------------------------------------------------2
+/*func main() {
+	var number1, number2 int
+
+	fmt.Print("Пожалуйста введите первое число: ")
+	fmt.Scanln(&number1)
+	fmt.Print("Пожалуйста введите второе число: ")
+	fmt.Scanln(&number2)
+
+	fmt.Printf("Сумма: %d\n", number1+number2)
+    fmt.Printf("Разность: %d\n", number1-number2)
+    fmt.Printf("Произведение: %d\n", number1*number2)
+	if number2 != 0 {
+		fmt.Println("Частное чисел", number1, "и", number2, "равна:", float64(number1)/float64(number2))
+	} else {
+		fmt.Println("Деление на 0 невозможно!")
+	}
+}*/
+
+//-----------------------------------------------------3
+/*
 func main() {
-	fmt.Println()
-}
+	var celsius float64
 
-var a, b, c string
-
-func test() {
-	var hello string
-	hello = "My name"
-	fmt.Println(hello)
-}
-
-func test1() {
-	var (
-		name string = "Mitya"
-		age  int    = 23
-	)
-	fmt.Println(name)
-	fmt.Println(age)
-}
-
-func integer() {
-	var a int = 1
-	fmt.Println(a)
-}
+	fmt.Println("Введите температуру в градусах Цельсия: ")
+	fmt.Scanln(&celsius)
+	//Фаренгейты = (Цельсий * 9/5) + 32, Кельвины = Цельсий + 273.15.
+	fmt.Println("Конвертация в Фаренгейт: ", float64(celsius)*9/5+32)
+	fmt.Println("Конвертация в Кельвины: ", float64(celsius)+273.15)
+}*/
