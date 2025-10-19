2039b54060dbbe9b1ade1af15295abf15d5faa4d
Ihido <ihido@bk.ru>
2025-10-19 18:32:24 +0500

    Решил ещё парочку задач

--- Tasks/task3/task3.go
+++ Tasks/task3/task3.go
@@ -1,8 +1,8 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
+/*func main() {
 	var num int
 	fmt.Println("Введите число: ")
 	fmt.Scanln(&num)
@@ -11,4 +11,35 @@ func main() {
 	} else {
 		fmt.Print("Число ", num, " не чётное!")
 	}
-}
+}*/
+
+/*func main() {
+	var a, b int
+	fmt.Println("Ввдите два числа через пробел: ")
+	fmt.Scanln(&a, &b)
+	if a >= b {
+		fmt.Println("Число ", a, " больше чем ", b)
+	} else {
+		fmt.Println("Число ", b, " больше чем ", a)
+	}
+}*/
+
+/*func main() {
+	var age int
+	fmt.Println("Введите Ваш возраст: ")
+	fmt.Scanln(&age)
+	if age <= 12 {
+		fmt.Println("Вы ребенок!")
+		return
+	}
+	if age <= 17 {
+		fmt.Println("Вы подросток!")
+		return
+	}
+	if age <= 65 {
+		fmt.Println("Вы взрослый!")
+		return
+	} else {
+		fmt.Println("Вы пожилой!")
+	}
+}*/
