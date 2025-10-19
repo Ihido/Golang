1f80d31a6f5dec2f376a5363e4b3749f14607e64
Ihido <ihido@bk.ru>
2025-10-19 21:54:33 +0500

    Добавил задачки и решил одну из них

---
+++ Tasks/task5/task5.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	var name, city string
+// 	var age int
+// 	fmt.Println("Введите Ваше имя: ")
+// 	fmt.Scanln(&name)
+// 	fmt.Println("Введите Ваш возраст: ")
+// 	fmt.Scanln(&age)
+// 	fmt.Println("Введите Ваш город: ")
+// 	fmt.Scanln(&city)
+// 	fmt.Println("Имя:", name, "Возраст:", age, "Город:", city)
+// }
+
+func main() {
+	var word string
+	var num int
+	fmt.Println("Введите слово: ")
+	fmt.Scanln(&word)
+	fmt.Println("Введите число: ")
+	fmt.Scanln(&num)
+
+}
